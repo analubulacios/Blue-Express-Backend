c3b72b53a5ac3ea5598c9e74cd75385c5b31817c
analubulacios <analubula87@gmail.com>
2024-02-26 11:41:06 -0300

    chore: format

--- src/modules/controllers/user.controller.ts
+++ src/modules/controllers/user.controller.ts
@@ -1,25 +1,25 @@
 import User from "../models/user.model";
 import { generateToken } from "../../utils.ts/generateToken";
-// import Url from "../models/url.model";
-// import { Op } from "sequelize";
+import Url from "../models/url.model";
+
+import { Op } from "sequelize";
 
 class UserController {
   async login(req: any, res: any) {
     try {
-      console.log("llegue al controller !!!!")
-      const {email } = req.body
+      const {email, localURLs} = req.body
       const [user, created] = await User.findOrCreate({ where: { email } });
       const userId = user.getDataValue('user_id')
 
-      // const localIds = localURLs.map((u: any) => u.url_id)
-      // await Url.update({"user_id": userId}, {
-      //   where: {
-      //     "user_id": null,
-      //     "url_id": {
-      //       [Op.in]: localIds
-      //     },
-      //   }
-      // })     
+      const localIds = localURLs?.map((u: any) => u.url_id) ?? [];  
+      await Url.update({"user_id": userId}, {
+        where: {
+          "user_id": null,
+          "url_id": {
+            [Op.in]: localIds
+          },
+        }
+      })  
 
       const token = generateToken(userId);
       return res.status(created ? 201 : 200).json({userId, token});
